25f3ee0cbba58332d7084cef7e4b96fdec1b2798
Maria Cabrera <mariacabrera03@gmail.com>
2022-05-05 15:45:40 -0300

    solucione el problema del captcha

--- js/formulario.js
+++ js/formulario.js
@@ -1,1 +1,23 @@
 "use strict";
+
+let numeroRandom = document.querySelector("#captcha");
+let boton = document.querySelector("#boton");
+boton.addEventListener("click", validar);
+
+function validar(){
+    let valor = document.querySelector("#valorCaptcha").value;
+    let mensaje = document.createAttribute("p");
+    document.querySelector("#form").appendChild(mensaje);
+    if (valor == numeroRandom.innerHTML){
+        mensaje.innerHTML = "El captcha es correcto"
+    } else {
+        mensaje.innerHTML = "El captcha es incorrecto"
+    }
+}
+
+
+function mostrarCaptcha(){
+    let numero = Math.floor(Math.random()*100);
+    numeroRandom.innerHTML = numero;
+}
+mostrarCaptcha();
